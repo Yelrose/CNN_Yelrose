c7bf0b5cb7629cfdbb0865fd772c30f884b03eef
hzhengjie <hzhengjie@ipin.com>
2015-06-08 18:54:15 +0800

    commit

--- include/layer.h
+++ include/layer.h
@@ -1,20 +1,38 @@
 #ifndef LAYER_H
 #define LAYER_H
 #include "debug.h"
+#include "blob.h"
+#include <iostream>
+using namespace std;
 
-enum layer_type ={
-    CONVOLUTION,
-    POOLING,
-    DATA,
-    LOSS
-};
 template <typename Dtype>
 class Layer {
     public:
+        Layer(){
+        }
+        void set_convolution_mode(int height,int weight,int stride,int feature_map) {
+            conv_height = height;
+            conv_weight = weight;
+            conv_stride = stride;
+            conv_feature = feature_map;
+            blobs_ = vector<Blob<Dtype> *> (conv_feature);
+            for(int i = 0;i < blobs_.size();i ++) {
+                blobs_[i] = new Blob(1,1,height,weight)
+            }
+        }
         void forward(const vector<Blob<Dtype> *>&bottom,const vector<Blob<Dtype>* > &top) {
+
+
         }
         void backward(const vector<Blob<Dtype> *>&bottom,const vector<Blob<Dtype>* > &top) {
+
         }
+    private:
+        vector<Blob<Dtype >* > blobs_;
+        int conv_height;
+        int conv_weight;
+        int conv_stride;
+        int conv_feature;
 };
 
 
